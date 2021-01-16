2ceaa05556074e214c6acc12ae0332f2e3f5ed5a
khxia <17xiak124268@gmail.com>
2021-01-16 01:03:42 -0800

    Initial camera setup

--- src/components/CameraScreen/CameraScreen.js
+++ src/components/CameraScreen/CameraScreen.js
@@ -1,22 +1,46 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import PropTypes from 'prop-types';
+import { Camera } from 'expo-camera';
 
 function CameraScreen({ navigation }) {
+    const [hasPermission, setHasPermission] = React.useState(null);
+    const [type, setType] = React.useState(Camera.Constants.Type.back);
+
+    React.useEffect(() => {
+        (async () => {
+          const { status } = await Camera.requestPermissionsAsync();
+          setHasPermission(status === 'granted');
+        })();
+    }, []);
+    
+    if (hasPermission === null) {
+        return <View/>
+    }
+    if (hasPermission === null) {
+        return <Text>You have not granted camera access permissions so this page is blank :(</Text>;
+    }
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Camera Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push('Camera')}
-        />
-        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-        <Button
-          title="Go back to first screen in stack"
-          onPress={() => navigation.popToTop()}
-        />
+      <View style={cam_styles.container}>
+        <Camera style={cam_styles.camera} type={type}>
+        <View style={cam_styles.buttonContainer}>
+
+        <TouchableOpacity style={cam_styles.button}
+            onPress={() => {
+                setType(type === Camera.Constants.Type.back 
+                        ? Camera.Constants.Type.front 
+                        : Camera.Constants.Type.back
+                );
+            }}
+        >
+            <Text style={cam_styles.text}>Flip</Text>
+        </TouchableOpacity>
+        
+        <Button style={cam_styles.button} title="Home" onPress={() => navigation.navigate('Home')} />
+        <Button style={cam_styles.button} title="Go back" onPress={() => navigation.goBack()} />
+        </View>
+        </Camera>
       </View>
     );
   }
@@ -26,6 +50,30 @@ CameraScreen.propTypes = {
         navigate: PropTypes.func.isRequired,
       }).isRequired,
 };
+
+const cam_styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      margin: 20,
+    },
+    button: {
+      flex: 0.1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 18,
+      color: 'white',
+    },
+  });
 
 export default CameraScreen
 
