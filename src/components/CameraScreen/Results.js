8bcfbaf87bfe6fbc15106e964c5e292b14717bac
James Ma <james@pop-os.localdomain>
2021-01-16 18:25:06 -0800

    added preliminary advice text logic

--- src/components/CameraScreen/Results.js
+++ src/components/CameraScreen/Results.js
@@ -22,6 +22,14 @@ function ResultsScreen({ navigation, route }) {
       "VERY_LIKELY":    "Very Likely"
     };
 
+    // emotions to likelihoods
+    const LHEmotions = {
+      "joy": "joyLikelihood",
+      "anger": "angerLikelihood",
+      "sorrow": "sorrowLikelihood",
+      "surprise": "surpriseLikelihood"
+    };
+
     let resultsText = (
       <>
         <Text>Joy:      N/A</Text>
@@ -52,20 +60,70 @@ function ResultsScreen({ navigation, route }) {
         </>
       );
     } else if(route.params.faces.length == 1) { // one face
+      const face = route.params.faces[0];
+
       resultsText = (
         <>
-          <Text>Joy:      {LHPhrase[route.params.faces[0].joyLikelihood]}</Text>
-          <Text>Anger:    {LHPhrase[route.params.faces[0].angerLikelihood]}</Text>
-          <Text>Sorrow:   {LHPhrase[route.params.faces[0].sorrowLikelihood]}</Text>
-          <Text>Surprise: {LHPhrase[route.params.faces[0].surpriseLikelihood]}</Text>
+          <Text>Joy:      {LHPhrase[face.joyLikelihood]}</Text>
+          <Text>Anger:    {LHPhrase[face.angerLikelihood]}</Text>
+          <Text>Sorrow:   {LHPhrase[face.sorrowLikelihood]}</Text>
+          <Text>Surprise: {LHPhrase[face.surpriseLikelihood]}</Text>
         </>
       )
+
+      let maxLikelihood = -1;
+      let maxEmotions = [];
+
+      // getting the most likely emotion(s)
+      for(const [emotion, LH] of Object.entries(LHEmotions)) {
+        if(LHNum[face[LH]] >= maxLikelihood) {
+          maxLikelihood = LHNum[face[LH]];
+          if(LHNum[face[LHEmotions[maxEmotions[maxEmotions.length - 1]]]] < maxLikelihood) { // remove previous
+            maxEmotions = [emotion];
+          } else {
+            maxEmotions.push(emotion);
+          }
+        }
+      }
+
+      console.log(maxLikelihood, maxEmotions);
+
+      // LHEmotions.forEach(emotion => {
+      //   if(LHNum[face[emotion]] >= maxLikelihood) {
+      //     maxLikelihood = LHNum[face[emotion]];
+      //     if(LHNum[face[maxEmotions[maxEmotions.length - 1]]] < maxLikelihood) { // remove previous
+      //       maxEmotions = [emotion];
+      //     } else {
+      //       maxEmotions.push(emotion);
+      //     }
+      //   }
+      // });
+
+      let advTxtLine1;
+
+      if(maxLikelihood == -1) {
+        advTxtLine1 = "WOW! It looks like all of this person's emotions are unknown! They're an enigma!";
+      } else if(maxLikelihood == 0) {
+        advTxtLine1 = "Looooooool. This person is feeling nothing right now. Completely empty inside. Nonthing's wrong with that though :)"
+      } else if(maxLikelihood == 1) {
+        advTxtLine1 = "Uuuh... it's not likely, but this person may be feeling ";
+      } else if(maxLikelihood == 2) {
+        advTxtLine1 = "It's definitely possible that this person is feeling ";
+      } else if(maxLikelihood == 3) {
+        advTxtLine1 = "It's likely that this person is feeling ";
+      } else if(maxLikelihood == 4) {
+        advTxtLine1 = "It's very likely that this person is feeling";
+      }
+
+      console.log(advTxtLine1);
+
+      // for loop to add advice here
       
       const detectionConfidenceString = (route.params.faces[0].detectionConfidence * 100).toFixed(0).toString();
       adviceText = (
         <>
           <Text>We are {detectionConfidenceString}% confident in our results.</Text>
-          <Text>Perhaps you should follow this advice:</Text>
+          <Text>{advTxtLine1}</Text>
         </>
       );
     }
